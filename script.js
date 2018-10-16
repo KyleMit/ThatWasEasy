e8763768fb16a06f80133203600ef102ec4badf5
Kyle <KyleMit@gmail.com>
2018-10-15 21:20:32 -0400

    add select sound picker

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-﻿var btn = document.getElementById('easyButton');
-var audio = document.getElementById('easyAudio');
+﻿
+//var audio = document.getElementById('easyAudio');
+var radios = document.querySelectorAll(".radio-btn");
+radios.forEach(function(rad) {
+   rad.addEventListener("click", function() {
+      radios.forEach(function(el) {el.classList.remove('active')});
+      this.classList.add('active');
+   });
+})
 
+var btn = document.getElementById('easyButton');
 btn.addEventListener("click", function () {
+    var audio = document.querySelector(".radio-btn.active audio");
     audio.play();
 });
